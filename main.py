48f24e33bbde8921e93608cae0e3f021e6508b8e
oprince-dev <oprince.tech@gmail.com>
2020-09-10 20:26:17 -0500

    UPDATE: Player Class inherits from Roster Class

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
 from bs4 import BeautifulSoup
+import operator
 
 class Colors:
     BLACK = '\033[90m'
     RED = '\033[91m'
     GREEN = '\033[32m'
+    BGREEN = '\033[92m'
     YELLOW = '\033[93m'
     BLUE = '\033[94m'
     MAGENTA = '\033[95m'
     CYAN = '\033[96m'
+    BWHITE = '\033[97m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-def main():
 
-    with open("table.html", "r") as f:
-        print("Reading data...")
-        data = f.read()
-        soup = BeautifulSoup(data, 'html.parser')
+class Roster:
+    def __init__(self):
+        self.roster = []
 
-    roster = []
-    def generate_players():
-        players = soup.find_all("tr", class_="Table__TR")
+    def __str__(self):
+        return self.roster
+
+    def generate_roster(self, players):
         print("Adding players to roster...")
         for i, player in enumerate(players):
-
-            # Conditional check for divider
             first_cell_class = player.findChild()['class']
             if 'fw-medium' in first_cell_class:
                 continue
@@ -37,89 +37,110 @@ def main():
             name = player_bio.a.text.strip()
             team = player_bio.find("span",
                                    class_="playerinfo__playerteam").text
+            slot = cells[0].div.text.strip()
             position = player_bio.find("span",
                                        class_="playerinfo__playerpos").text
             projection = cells[5].span.text
             inj_rpt = player_bio.find("span",
                                       class_="playerinfo__injurystatus")
             status = inj_rpt.text.strip() if inj_rpt else "OK"
-
-            # 'Starting' check
-            starting = 'S' if i <= 8 else 'B'
-
-            # Create instance of player class and add to list of players
-            p = {
-                    "Name": name,
-                    "Team": team,
-                    "Position": position,
-                    "Projection": float(projection),
-                    "Starting": starting,
-                    "Status": status
-                    }
-            roster.append(p)
+            player = Player(name, team, slot, position,
+                            projection, status)
+            self.roster.append(player)
 
 
-    def projection_check(*position):
-        #search position best projection is starting
-        if position:
-            prj_players = list(filter(lambda p: p['Position'] == position[0],
-                                      roster))
-        else:
-            prj_players = roster
-        prj_players.sort(key=lambda proj: proj.get('Projection'),
-                               reverse=True)
-        prj_players.sort(key=lambda strt: strt.get('Starting'),
-                               reverse=True)
-        for p in prj_players:
-            position, name, projection, starting, status = [
-                    p['Position'], p['Name'], p['Projection'],
-                    p['Starting'], p['Status']
-                    ]
+    def decide_flex(self):
+        flex_ok = ["RB", "WR", "TE"]
+        flex_spot = list(
+                filter(lambda x: x.position in flex_ok and not x.start,
+                self.roster))
+        flex = flex_spot[0]
+        flex.start = True
 
-            color_starting = {"S": Colors.BLUE, "B": Colors.BLACK}
-            color_status = {
-                    "OK": Colors.GREEN,
-                    "Q": Colors.YELLOW,
-                    "O": Colors.RED,
-                    "IR": Colors.RED
-                    }
-            line_format = f"{color_starting[starting]}" \
-                          f"{position}" \
-                          f"{Colors.ENDC}: " \
-                          f"{color_status[status]}" \
-                          f"{name}" \
-                          f"{Colors.ENDC} " \
-                          f"({projection})"
-            print(line_format)
-
-        get_user_input()
-
-    def injury_report():
-        #makes sure bench doesn't have injured players
-        get_user_input()
+        return flex
 
 
-    def get_user_input():
-        user_input = input("\nWhat is your action?: ").upper()
-        user_args = user_input.split(" ")
-        print()
-        if user_args[0] == "PROJECTION":
-            try:
-                position = user_args[1]
-                print(f"Generating {position} projections... \n")
-                projection_check(position)
-            except:
-                print("Generating full roster projections... \n")
-                projection_check()
-        elif user_args[0] == "INJURY":
-            print("Generating injury report")
-            injury_report()
-        else:
-            print("Error")
+    def decide_lineup(self):
+        position_spots = {
+                "QB": 1,
+                "RB": 2,
+                "WR": 2,
+                "TE": 1,
+                "D/ST": 1,
+                "K": 1,
+                "FLEX": 1
+                }
+        for pos, num in position_spots.items():
+            position_players = list(filter(lambda x: x.position == pos,
+                                    self.roster))
+            if pos == "FLEX":
+                flex = self.decide_flex()
+                position_players.append(flex)
+
+            for i in range(num):
+                try:
+                    p = position_players[i]
+                    p.start = True
+                except:
+                    print(f"Skipping {pos}")
 
 
-    generate_players()
-    get_user_input()
+class Player(Roster):
+    def __init__(self, name, team, slot, position, projection, status):
+        self.first = name.split(" ")[0]
+        self.last = name.split(" ")[1]
+        self.team = team
+        self.slot = slot
+        self.position = position
+        self.projection = float(projection)
+        self.status = status
+        self.start = False
+
+    def apply_color(self):
+        color_start = {True: Colors.BLUE, False: Colors.BLACK}
+        color_slot = {"Bench": Colors.BLACK}
+        color_status = {
+        "OK": Colors.GREEN,
+        "Q": Colors.YELLOW,
+        "O": Colors.RED,
+        "IR": Colors.RED
+        }
+        self.color_start = color_start[self.start]
+        self.color_status = color_status[self.status]
+        self.color_slot = color_slot[self.slot] if self.slot == "Bench" else Colors.BWHITE
+
+    def __repr__(self):
+        self.apply_color()
+        return f"\n{self.color_start}" \
+               f"{self.position}" \
+               f"{Colors.ENDC}:\t" \
+               f"{self.color_status}" \
+               f"{self.first[0]}. " \
+               f"{self.last}  " \
+               f"{Colors.ENDC}\t" \
+               f"{self.color_slot}" \
+               f"({self.projection})" \
+               f"{Colors.ENDC}"
+
+
+def get_user_input():
+    user_input = input("\nWhat is your action?: ").upper()
+
+    if user_input == "PROJECTION":
+        print(f"Generating projections... \n")
+        myTeam.projection_check()
+    else:
+        print("Invalid Command")
+
 
 if __name__ == '__main__':
-    main()
+    with open("table.html", "r") as f:
+        print("Reading data...")
+        data = f.read()
+        soup = BeautifulSoup(data, 'html.parser')
+
+    players = soup.find_all("tr", class_="Table__TR")
+    myTeam = Roster()
+    myTeam.generate_roster(players)
+    myTeam.decide_lineup()
+    print(myTeam.roster)
