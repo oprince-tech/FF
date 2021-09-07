bcf173eaa996bb4cba7557c46e45e4824241b42a
Oliver Prince <oprince.tech@gmail.com>
2021-09-07 15:34:12 -0500

    ADDED: Argument parsing and read from file

---
+++ TODO.md
@@ -0,0 +1,7 @@
+### Todo
+- [ ] (_Fri 09/03/21, 14:23:51_ ) - argparse
+- [ ] (_Fri 09/03/21, 14:24:44_ ) - Connect to API and save json local. Use local if up to date
+- [ ] (_Fri 09/03/21, 14:25:20_ ) - packaging
+- [ ] (_Fri 09/03/21, 15:05:43_ ) - Add owner to print_roster
+### In Progress
+### Completed
--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-import operator
-from typing import Dict
-import requests
+import argparse
 import json
+import operator
+import os
+import re
+import sys
+
+import requests
+# from pathlib import Path
 
 
 slotID = {
-        0: 'QB', 2: 'RB', 4: 'WR',
-        6: 'TE', 16: 'DST', 17: 'K',
-        20: 'Bench', 21: 'IR', 23: 'FLEX'
+    0: 'QB', 2: 'RB', 4: 'WR',
+    6: 'TE', 16: 'DST', 17: 'K',
+    20: 'Bench', 21: 'IR', 23: 'FLEX',
 }
 
 positionID = {
-        1: 'QB', 2: 'RB', 3: 'WR',
-        4: 'TE', 5: 'K', 16: 'DST'
+    1: 'QB', 2: 'RB', 3: 'WR',
+    4: 'TE', 5: 'K', 16: 'DST',
 }
 
 
@@ -30,11 +35,11 @@ class Colors:
 
 
 class Roster:
-    def __init__(self):
+    def __init__(self) -> None:
         self.roster = []
 
-    def generate_roster(self, json_roster, TID):
-        print("\nAdding players to roster...")
+    def generate_roster(self, d, TID):
+        print('\nAdding players to roster...')
         for team in d['teams']:
             if team['id'] == TID:
                 for p in team['roster']['entries']:
@@ -42,7 +47,10 @@ class Roster:
                     slot = slotID[p['lineupSlotId']]
                     slot_id = p['lineupSlotId']
                     starting = False if slot == 'Bench' else True
-                    pos = positionID[p['playerPoolEntry']['player']['defaultPositionId']]
+                    pos = positionID[
+                        p['playerPoolEntry']
+                        ['player']['defaultPositionId']
+                    ]
                     proj, score = 0, 0
                     for stat in p['playerPoolEntry']['player']['stats']:
                         if stat['scoringPeriodId'] != 2:
@@ -55,26 +63,29 @@ class Roster:
                     status = 'ACTIVE'
                     try:
                         status = p['playerPoolEntry']['player']['injuryStatus']
-                    except:
+                    except KeyError:
                         pass
-
-                    player = Player(name, slot, slot_id, pos, starting, proj, score, status)
+                    player = Player(
+                        name, slot, slot_id, pos,
+                        starting, proj, score, status,
+                    )
 
                     self.roster.append(player)
-
 
     def sort_roster(self):
         self.roster.sort(key=operator.attrgetter('slot_id'))
 
-
     def decide_flex(self):
-        print("Deciding flex position...")
-        flex_ok = ["RB", "WR", "TE"]
+        print('Deciding flex position...')
+        flex_ok = ['RB', 'WR', 'TE']
         flex_spot = list(
-                    filter(lambda x: x.pos in flex_ok and not x.starting,
-                    self.roster))
+            filter(
+                lambda x: x.pos in flex_ok and not x.starting,
+                self.roster,
+            ),
+        )
         flex = flex_spot[0]
-        current_flex = list(filter(lambda x: x.slot == "FLEX", self.roster))
+        current_flex = list(filter(lambda x: x.slot == 'FLEX', self.roster))
 
         if flex.proj > current_flex[0].proj:
             flex.shouldStart = True
@@ -87,20 +98,23 @@ class Roster:
         flex.slot_id = 15
         return flex
 
-
     def decide_lineup(self):
-        print("Deciding best lineup...")
+        print('Deciding best lineup...')
         position_spots = {
-                "QB": 1, "RB": 2, "WR": 2,
-                "TE": 1, "DST": 1, "K": 1,
-                "FLEX": 1
-                }
+            'QB': 1, 'RB': 2, 'WR': 2,
+            'TE': 1, 'DST': 1, 'K': 1,
+            'FLEX': 1,
+        }
 
         for pos, num in position_spots.items():
-            position_players = list(filter(lambda x: x.pos == pos,
-                                    self.roster))
+            position_players = list(
+                filter(
+                    lambda x: x.pos == pos,
+                    self.roster,
+                ),
+            )
 
-            if pos == "FLEX":
+            if pos == 'FLEX':
                 flex = self.decide_flex()
                 position_players.append(flex)
                 continue
@@ -109,21 +123,26 @@ class Roster:
                 try:
                     p = position_players[i]
                     p.shouldStart = True
-                except:
-                    print(f"Skipping {pos}")
+                except IndexError:
+                    print(f'Skipping {pos}')
 
     def print_roster(self):
-        header = ("\n{}{:>12}{:>18}{:>12}").format('Slot', 'Player', 'Proj', 'Score')
+        header = ('\n{}{:>12}{:>18}{:>12}').format(
+            'Slot', 'Player', 'Proj', 'Score',
+        )
         print(header)
-        print("----------------------------------------------")
+        print('----------------------------------------------')
         for p in self.roster:
             print(p)
 
 
 class Player(Roster):
-    def __init__(self, name, slot, slot_id, pos, starting, proj, score, status):
-        self.first = name.split(" ")[0]
-        self.last = name.split(" ")[1]
+    def __init__(
+        self, name, slot, slot_id, pos,
+        starting, proj, score, status,
+    ):
+        self.first = name.split(' ')[0]
+        self.last = name.split(' ')[1]
         self.slot = slot
         self.slot_id = slot_id
         self.pos = pos.upper()
@@ -137,92 +156,128 @@ class Player(Roster):
         color_starting = {True: Colors.BLUE, False: Colors.BLACK}
         color_shouldStart = {True: Colors.CYAN, False: Colors.BLACK}
         color_status = {
-                        "ACTIVE": Colors.GREEN,
-                        "QUESTIONABLE": Colors.YELLOW,
-                        "OUT": Colors.RED,
-                        "IR": Colors.RED
-                        }
+            'ACTIVE': Colors.GREEN,
+            'QUESTIONABLE': Colors.YELLOW,
+            'OUT': Colors.RED,
+            'IR': Colors.RED,
+        }
         self.color_starting = color_starting[self.starting]
         self.color_status = color_status[self.status]
         self.color_shouldStart = color_shouldStart[self.shouldStart]
 
-
     def __str__(self):
         self.apply_color()
-        return f"{self.color_starting}" \
-               f"{self.slot}" \
-               f"{Colors.ENDC}:\t" \
-               f"{self.color_status :<8}" \
-               f"{self.first[0]}. " \
-               f"{self.last}" \
-               f"{Colors.ENDC}\t" \
-               f"{self.color_shouldStart}" \
-               f"({self.proj})" \
-               f"{Colors.ENDC}\t" \
-               f"{self.score :>5}".expandtabs(tabsize=16)
+        return f'{self.color_starting}' \
+               f'{self.slot}' \
+               f'{Colors.ENDC}:\t' \
+               f'{self.color_status :<8}' \
+               f'{self.first[0]}. ' \
+               f'{self.last}' \
+               f'{Colors.ENDC}\t' \
+               f'{self.color_shouldStart}' \
+               f'({self.proj})' \
+               f'{Colors.ENDC}\t' \
+               f'{self.score :>5}'.expandtabs(tabsize=16)
 
 
-def ID_check(LID, TID):
-    try:
-        LID = int(LID)
-        TID = int(TID)
-    except:
-        raise TypeError("ID's must be a number")
-
-    if not len(str(LID)) == 6:
-        raise TypeError("League ID must have a length of 6")
-    if not TID <= 12:
-        raise TypeError(f"No team with a Team ID of {TID} was found")
-
-def get_user_input():
-    try:
-        LID = input("League ID: ")
-        TID = input("Team ID: ")
-        ID_check(LID, TID)
-        return {"LID": int(LID), "TID": int(TID)}
-    except TypeError as e:
-        print(f"{Colors.RED}Error: {e}{Colors.ENDC}")
-        quit()
-
-def connect_FF(ID: Dict[str, int]):
-    with open("cookies.json", "r") as rc:
+def load_cookies(key=None):
+    with open('cookies-dev.json') as rc:
         c = json.load(rc)
+    return c[key] if key else c
 
-    swid = c['SWID']
-    espn_s2 = c['espn_s2']
-    url = f"https://fantasy.espn.com/apis/v3/games/ffl/seasons/2020/segments/0/leagues/{ID['LID']}?view=mMatchup&view=mMatchupScore"
-
+def save_weekly_data(d, wk):
     try:
-        r = requests.get(url, params={'scoringPeriodId': 2},
-                              cookies={"SWID": swid, "espn_s2": espn_s2})
-        d = r.json()
-        return d
-
-    except requests.exceptions.RequestException as e:
-        raise SystemExit(e)
-
-
-if __name__ == '__main__':
-    ID = get_user_input()
-
-    d = connect_FF(ID)
-    wk = d['scoringPeriodId']
-
-    try:
-        with open(f"weekly_data/FF_wk-{wk}.json", "w") as wf:
+        with open(f"./weekly_data/FF_wk-{wk}.json", "w") as wf:
             json.dump(d, wf)
     except Exception as e:
         print(e)
 
+def connect_FF(LID: int, wk: int) -> dict[str, str]:
+    c = load_cookies()
+    swid = c['SWID']
+    espn_s2 = c['espn_s2']
+    url = f'''https://fantasy.espn.com/apis/v3/games/ffl/seasons/2020/
+              segments/0/leagues/{LID}?view=mMatchup&view=mMatchupScore'''
 
     try:
-        with open("weekly_data/FF_wk-2.json", "r") as rf:
-            d = json.load(rf)
-    except Exception as e:
-        print(e)
+        r = requests.get(
+            url, params={'scoringPeriodId': wk},
+            cookies={'SWID': swid, 'espn_s2': espn_s2},
+        )
+        d = r.json()
+        return d
+
+    except requests.exceptions.RequestException as e:
+        sys.exit(f'{Colors.RED}{type(e).__name__}: {e}{Colors.ENDC}')
+
+
+def last_updated_week() -> int:
+    wks = os.listdir('./weekly_data')
+    most_current = 0
+    for wk in wks:
+        wk_num = int(re.search(r'wk-(\d)', wk).group(1))
+        most_current = wk_num if wk_num > most_current else most_current
+    return int(most_current)
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='FF CLI')
+    parser.add_argument(
+        '-p', '--pull',
+        help='Update data (pull from API)',
+        action='store_true',
+    )
+    parser.add_argument(
+        '-w', '--week',
+        help='Specify week',
+        type=int,
+    )
+    parser.add_argument(
+        '-l', '--league-id',
+        help='League ID',
+        type=int,
+    )
+    parser.add_argument(
+        '-t', '--team-id',
+        help='Team ID',
+        type=int,
+    )
+    parser.add_argument(
+        '-m', '--matchup',
+        help='Show your matchup',
+        action='store_true',
+    )
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    if not args.week:
+        args.week = last_updated_week()
+    if not args.league_id:
+        args.league_id = int(load_cookies(key='LID'))
+    if not args.team_id:
+        args.team_id = int(load_cookies(key='TID'))
+    if not args.pull:
+        try:
+            with open(
+                '''./weekly_data/FF_2020_'''
+                f'''{args.league_id}_wk-{args.week}.json''',
+            ) as rf:
+                d = json.load(rf)
+        except Exception as e:
+            print(e)
+    else:
+        d = connect_FF(args.league_id, args.week)
+        save_weekly_data(d)
 
     myTeam = Roster()
-    myTeam.generate_roster(d, ID['TID'])
+    myTeam.generate_roster(d, args.team_id)
     myTeam.decide_lineup()
     myTeam.sort_roster()
     myTeam.print_roster()
+
+
+if __name__ == '__main__':
+    main()
